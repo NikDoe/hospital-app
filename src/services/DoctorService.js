f168b3a936239f291391fae899361f71b4d5adaa
NikDoe <nikd0e@yandex.ru>
2022-09-09 22:01:15 +0300

    feature(h4s): add get all doctor service

--- src/services/DoctorService.js
+++ src/services/DoctorService.js
@@ -1,10 +1,15 @@
-import {doctorRepo} from "../utils/utils.js";
+import { doctorRepo } from '../utils/utils.js';
 
 class DoctorService {
 	async createDoctor (doctor) {
 		const newDoctor = doctorRepo.create(doctor);
 		await doctorRepo.save(newDoctor);
 		return newDoctor;
+	}
+
+	async getAllDoctors () {
+		const allDoctors = await doctorRepo.find();
+		return allDoctors;
 	}
 }
 
