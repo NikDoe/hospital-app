48340a886f5127c46b61d987281d9c5b37bc1265
NikDoe <nikd0e@yandex.ru>
2022-09-09 14:57:12 +0300

    feature(h4s): DB connection & home route

--- server/src/index.js
+++ server/src/index.js
@@ -2,6 +2,7 @@ import 'dotenv/config';
 import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
+import { AppDataSource } from './db.js';
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -9,8 +10,17 @@ const PORT = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(cors());
 
+app.get('/', (req, res) => {
+	res.send('<h1>hospital app</h1>');
+});
+
 const start = async () => {
 	try {
+		await AppDataSource.initialize()
+			.then(() => {
+				console.log('база данных подключена');
+			})
+			.catch(error => console.log(error));
 		await app.listen(PORT, () => {
 			console.log(`сервер запущен на порту ${PORT}`);
 		});
